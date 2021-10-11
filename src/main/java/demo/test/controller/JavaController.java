d62ffc940e2fd0fa4776794647f84757e2a4d12c
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-10-11 20:39:18 +0700

    fix: for deploying to Heroku

--- src/main/java/demo/test/controller/JavaController.java
+++ src/main/java/demo/test/controller/JavaController.java
@@ -3,7 +3,6 @@ package demo.test.controller;
 import demo.test.model.JavaObj;
 import demo.test.repository.JavaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +15,6 @@ public class JavaController {
     @GetMapping(path = "/all")
     public @ResponseBody
     Iterable<JavaObj> getAllUsers() {
-        System.out.println(userRepository.count());
-
         JavaObj obj = new JavaObj();
         obj.setName("Test 1");
 
