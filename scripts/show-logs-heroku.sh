b21f64e80accf40d77cdc377abedf552ceea01c7
thanhdat1902 <thanhdat19022001@gmail.com>
2021-10-11 20:39:18 +0700

    fix: for deploying to Heroku

---
+++ scripts/show-logs-heroku.sh
@@ -0,0 +1,4 @@
+heroku logs --tail --app spring-demo-5464
+
+# Usage of ClearDB MySQL on Heroku
+# https://www.cleardb.com/database/details?id=5B9C452A4ADFBC1B7EBFCB8F622AD34B#
--- src/main/java/demo/test/DemoApplication.java
+++ src/main/java/demo/test/DemoApplication.java
@@ -1,23 +1,13 @@
 package demo.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Autowired
-	private Environment env;
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("@@_Db_url: " + env.getProperty("spring.datasource.url"));
-	}
 }
