237b60aa1fcdf8784ff93c972e4b4b1d61aabfee
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2021-10-17 15:33:38 +0700

    format code

--- src/main/java/demo/test/controller/LoginController.java
+++ src/main/java/demo/test/controller/LoginController.java
@@ -1,18 +1,12 @@
 package demo.test.controller;
 
-import demo.test.model.JavaObj;
 import demo.test.model.RequestLogin;
-import demo.test.repository.JavaRepository;
 import demo.test.service.LoginService;
-import net.minidev.json.JSONObject;
-import netscape.javascript.JSObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/signup")
@@ -21,7 +15,8 @@ public class LoginController {
     LoginService loginService;
 
     @PostMapping(value = "/getEmail")
-    public @ResponseBody RequestLogin getEmail(@RequestBody RequestLogin requestLogin) {
+    public @ResponseBody
+    RequestLogin getEmail(@RequestBody RequestLogin requestLogin) {
         System.out.println(requestLogin.getEmail());
         return requestLogin;
     }
