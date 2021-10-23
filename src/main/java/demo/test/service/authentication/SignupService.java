4fe56e13109a40c949d6899270c426155da642fd
nctai19125120 <63503421+nctai19125120@users.noreply.github.com>
2021-10-23 17:38:42 +0700

    update: move login into signup service

--- src/main/java/demo/test/service/authentication/SignupService.java
+++ src/main/java/demo/test/service/authentication/SignupService.java
@@ -1,6 +1,7 @@
 package demo.test.service.authentication;
 
 import demo.test.constant.AuthenticationEnum;
+import demo.test.model.response.BaseResponse;
 import demo.test.service.database.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,21 @@ public class SignupService {
             profileService.createAccount(username, password, email);
         }
         return true;
+    }
+
+    public BaseResponse handleEmailFacebook(String email) {
+        AuthenticationEnum status = AuthenticationEnum.FACEBOOK_SUCCESS;
+        if (email == null || email.equals("")) {
+            status = AuthenticationEnum.FACEBOOK_FAIL;
+        }
+        if (profileService.existingEmail(email)) {
+            status = AuthenticationEnum.EXISTING_EMAIL;
+        }
+        return BaseResponse.Builder()
+                .addStatus(status == AuthenticationEnum.FACEBOOK_SUCCESS)
+                .addCode(status.getDescCode())
+                .addDesc(status.getDesc())
+//                .addData(res)
+                .build();
     }
 }
