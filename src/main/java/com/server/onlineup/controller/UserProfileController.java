6f52594e7dd9efcb9bece5b70a532de8c0eed73d
thanhdat1902 <thanhdat19022001@gmail.com>
2021-11-01 21:19:01 +0700

    update: rename Controller

--- src/main/java/com/server/onlineup/controller/UserProfileController.java
+++ src/main/java/com/server/onlineup/controller/UserProfileController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @CrossOrigin
 @RequestMapping("/forget-password")
-public class ForgetPasswordController {
+public class UserProfileController {
 
     @Autowired
     ChangePasswordService changePasswordService;
